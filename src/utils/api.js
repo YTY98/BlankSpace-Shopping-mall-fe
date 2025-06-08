ac647246b65a9da120399ca6b01a1de6e02bf75e
taeYoung Yang <xodud120016@gmail.com>
2025-06-08 15:26:21 +0900

    카트추가오류수정

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,7 +8,7 @@ const api = axios.create({
   baseURL: LOCAL_BACKEND,
   headers: {
     "Content-Type": "application/json",
-    authorization: `Bearer ${sessionStorage.getItem("token")}`,
+    authorization: `Bearer ${localStorage.getItem("token")}`,
   },
 });
 
